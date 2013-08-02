6fcb74a1cdf32e1811c7f3edf2f358903671e96d
Spencer Alderman <spam.mehere@hotmail.com>
2013-08-02 11:33:43 -0400

    Incrementing magically fixed

--- src/main/java/com/rogue/playtime/data/yaml/YAML.java
+++ src/main/java/com/rogue/playtime/data/yaml/YAML.java
@@ -33,11 +33,11 @@ import org.bukkit.configuration.file.YamlConfiguration;
  * @version 1.4.0
  */
 public class YAML {
-    
+
     private YamlConfiguration yaml;
     private final String path;
     private Playtime plugin = Playtime.getPlugin();
-    
+
     public YAML() {
         path = plugin.getDataFolder() + File.separator + "users.yml";
         File users = new File(path);
@@ -47,50 +47,53 @@ public class YAML {
             Logger.getLogger(YAML.class.getName()).log(Level.SEVERE, plugin.getCipher().getString("data.yaml.instance.error-create"), ex);
         }
     }
-    
+
     /**
-     * Increases an int value within the data file by 1, and fires events if triggered
-     * 
+     * Increases an int value within the data file by 1, and fires events if
+     * triggered
+     *
      * For the users yml file, the key should be in the format:
      * "users.[username].[value]"
-     * 
+     *
      * @since 1.3.0
      * @version 1.4.0
-     * 
+     *
      * @param key The path to the data to edit
      */
     public void incrementValue(String key) {
         int i = yaml.getInt(key);
         i++;
-        boolean eventFired = false;
-        List<String> fire = new ArrayList();
-        for (Event e : plugin.getEventHandler().getEvents().values()) {
-            if (!e.isLoginEvent() && i == e.getTrigger()) {
-                eventFired = true;
-                fire.add(e.getName());
+        if (plugin.getEventHandler() != null) {
+            boolean eventFired = false;
+            List<String> fire = new ArrayList();
+            for (Event e : plugin.getEventHandler().getEvents().values()) {
+                if (!e.isLoginEvent() && i == e.getTrigger()) {
+                    eventFired = true;
+                    fire.add(e.getName());
+                }
             }
-        }
-        if (eventFired) {
-            plugin.getEventHandler().fireEvents(fire, key.split(".")[1]);
+            if (eventFired) {
+                plugin.getEventHandler().fireEvents(fire, key.split(".")[1]);
+            }
         }
         yaml.set(key, i);
     }
-    
+
     /**
      * Returns a YamlConfiguration of the data file.
-     * 
+     *
      * @since 1.3.0
      * @version 1.3.0
-     * 
+     *
      * @return The YamlConfiguration
      */
     public YamlConfiguration getFile() {
         return yaml;
     }
-    
+
     /**
      * Forces a save of the current yaml data file in memory.
-     * 
+     *
      * @since 1.3.0
      * @version 1.3.0
      */
@@ -101,17 +104,17 @@ public class YAML {
             Logger.getLogger(YAML.class.getName()).log(Level.SEVERE, plugin.getCipher().getString("data.yaml.instance.error-save"), ex);
         }
     }
-    
+
     /**
      * Creates a YAML file if none exists yet, and returns it as a
      * YamlConfiguration
-     * 
+     *
      * @since 1.3.0
      * @version 1.3.0
-     * 
+     *
      * @param yaml The path to the file
      * @return the file path made into a YamlConfiguratino
-     * @throws IOException 
+     * @throws IOException
      */
     private YamlConfiguration makeYAML(File yaml) throws IOException {
         if (yaml.exists()) {
@@ -120,5 +123,4 @@ public class YAML {
         yaml.createNewFile();
         return YamlConfiguration.loadConfiguration(yaml);
     }
-
 }
